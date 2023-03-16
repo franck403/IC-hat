903c2692c75a7f41580aaa542e9e7c077fcb3fa3
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-16 13:56:02 -0400

    dsdsdsdsdsd

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { initializeApp,firebase } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
 import {
     getDatabase,
-    firebase,
     set,
     ref,
     push,
