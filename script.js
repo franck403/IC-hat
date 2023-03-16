4659f01787bb53c4ac7e7de587f6fb0b4f9a1bfc
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-16 12:17:07 -0400

    sdsssd

--- script.js
+++ script.js
@@ -52,3 +52,27 @@ onChildAdded(newMsg, (data) => {
         d1.insertAdjacentHTML('chat', html);
     }
 });
+
+
+ // FirebaseUI config.
+ var uiConfig = {
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: 'https://splendorous-hamster-ecd34b.netlify.app/',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function() {
+      window.location.assign('https://splendorous-hamster-ecd34b.netlify.app/');
+    }
+  };
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
