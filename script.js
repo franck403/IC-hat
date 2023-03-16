4fe073dcdc7241c029fe0ae7d9f39a1fd448de16
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-16 12:01:52 -0400

    sdsds

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+  authDomain: "ic-hat.firebaseapp.com",
+  databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+  projectId: "ic-hat",
+  storageBucket: "ic-hat.appspot.com",
+  messagingSenderId: "720687529085",
+  appId: "1:720687529085:web:2d964e880c5e2398058514",
+  measurementId: "G-YC8K0D7GLR"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firebaseConfig = {
+// your config code
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+var myName = prompt("Enter your name");
+
 function send() {
     var message = document.getElementById("content")
     html = `<div class="bubble me">${ message.value }</div>`
