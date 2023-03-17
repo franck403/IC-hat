b8c568184c5c27515bf3032a29719b30e8fda282
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-17 10:58:05 -0400

    ygygtyg

--- index.js
+++ index.js
@@ -60,21 +60,9 @@ var firebaseConfig = {
       database_ref.child('users/' + user.uid).set(user_data)
   
       // DOne
-      const dbRef = ref(getDatabase());
-      get(child(dbRef, `users/${user.uid}`)).then((snapshot) => {
-        if (snapshot.exists()) {
-          var username = snapshot.val().full_name
-          var email = snapshot.val().email
-        } else {
-          console.log("No data available");
-        }
-      }).catch((error) => {
-        console.error(error);
-      });
 
-      localStorage.setItem("name",username)
-      localStorage.setItem("email",email)
-      window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/");
+      localStorage.setItem("uid",user.uid)
+      window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/end");
     })
     .catch(function(error) {
       // Firebase will use this to alert of its errors
