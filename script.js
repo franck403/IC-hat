0af1c655981cc023279afdff94b0faba1206d3c6
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-17 17:33:45 -0400

    sdsdd

--- script.js
+++ script.js
@@ -48,6 +48,32 @@ var friendhtml = `
 const send = document.getElementById("send");
 const send2 = document.getElementById("content");
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 send.addEventListener('click', (e) => {
     var fg = document.getElementById('content').value
     var gh = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
