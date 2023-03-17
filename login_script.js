c44980ff1e7b519b6d7d147d9ecc0fb05293183c
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-17 11:00:46 -0400

    dfdfd

--- login_script.js
+++ login_script.js
@@ -9,9 +9,7 @@ import {
     onValue,
     onChildAdded
 } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-
-
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
   authDomain: "ic-hat.firebaseapp.com",
   databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
@@ -21,12 +19,9 @@ var firebaseConfig = {
   appId: "1:720687529085:web:2d964e880c5e2398058514",
   measurementId: "G-YC8K0D7GLR"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// Initialize variables
-const auth = firebase.auth()
-const database = firebase.database()
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 var user = localStorage.getItem("uid")
 
