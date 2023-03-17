6cbc08483f225f11bdba7db2f963e4e85891e5c9
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-17 17:38:58 -0400

    ssdsd

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const send2 = document.getElementById("content");
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("new_friend");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
