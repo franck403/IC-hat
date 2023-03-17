ab193bee6f75e6613532bbf019f20f23efa04505
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-17 10:58:58 -0400

    dfdfdf

--- login_script.js
+++ login_script.js
@@ -11,6 +11,23 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
 
 
+var firebaseConfig = {
+  apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+  authDomain: "ic-hat.firebaseapp.com",
+  databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+  projectId: "ic-hat",
+  storageBucket: "ic-hat.appspot.com",
+  messagingSenderId: "720687529085",
+  appId: "1:720687529085:web:2d964e880c5e2398058514",
+  measurementId: "G-YC8K0D7GLR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth()
+const database = firebase.database()
+
+
 var user = localStorage.getItem("uid")
 
 const dbRef = ref(getDatabase());
