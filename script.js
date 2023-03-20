59d7eba3f4443d752a206ba7c581cfd839248ef3
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-20 15:03:26 -0400

    dsds

--- script.js
+++ script.js
@@ -141,6 +141,7 @@ send2.addEventListener("keydown", (e) => {
 });
 
 const newMsg = ref(database, 'messages/geoloup_chat/');
+var romc = ref(database, 'messages/geoloup_chat/');
 onChildAdded(newMsg, (data) => {
     if(data.val().name != myName) {
         var html = `<div class="bubble you">${ data.val().message }</div>`
@@ -186,6 +187,26 @@ onChildAdded(friend_invite, (data) => {
         const d2 = document.getElementById("people")
         d1.innerHTML = d1.innerHTML + html_chat
         d2.innerHTML = d2.innerHTML + html
+        var romc = ref(database, `messages/${data.val().dname}`);
+        onChildAdded(romc, (data2) => {
+            if(data2.val().name != myName) {
+                var html = `<div class="bubble you">${ data2.val().message }</div>`
+                const d1 = document.querySelector('[data-chat=""]');
+                d1.innerHTML = d1.innerHTML + html
+                document.getElementById("time").innerHTML =  data2.val().date
+                document.getElementById("prew").innerHTML =  data2.val().message
+            }else{
+                var html = `<div class="bubble me">${ data2.val().message }</div>`
+                const d1 = document.querySelector(`[data-chat="${data2.val().dname}"]`);
+                d1.innerHTML = d1.innerHTML + html
+                document.getElementById("time").innerHTML =  data2.val().date
+                document.getElementById("prew").innerHTML =  data2.val().message
+            }
+            var elem = document.querySelector('[data-chat="person1"]');
+            elem.scrollTop = elem.scrollHeight;
+            elem.scrollTop = elem.scrollHeight;
+        });
+        
     }else{}
     
 });
