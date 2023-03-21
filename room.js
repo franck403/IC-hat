2130ac699bb5a57076fe3da245b90826b6061618
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-21 18:27:51 -0400

    sdsdsdsd

--- room.js
+++ room.js
@@ -7,8 +7,8 @@ function room(id){
     var old1 = document.getElementsByClassName("person active")[0]
     var old2 = document.getElementsByClassName("chat active-chat")[0]
     var new1 = document.getElementById(id)
-    old1.attribute("class","person")
-    old2.attribute("class","chat")
-    new1.attribute("class","person active")
-    new2.attribute("class","chat active-chat")
+    old1.setattribute("class","person")
+    old2.setattribute("class","chat")
+    new1.setattribute("class","person active")
+    new2.setattribute("class","chat active-chat")
 }
