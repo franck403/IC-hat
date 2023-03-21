c40629c97979edb3088954dd90211c123cc55d44
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-21 17:56:16 -0400

    dsdsdsdsdsds

--- room.js
+++ room.js
@@ -1,9 +1,13 @@
 function room(id){
+    if (id == "geoloup_chat") {
+        var new2 = document.getElementById("room_" + id + "_c")        
+    } else {
+        var new2 = document.getElementById("room_" + id)
+    }
     var old1 = document.getElementsByClassName("person active")[0]
     var old2 = document.getElementsByClassName("chat active-chat")[0]
     var new1 = document.getElementById(id)
-    var new2 = document.getElementById("room_" + id)
-    old1.attributes("class","person")
+s    old1.attributes("class","person")
     old2.attributes("class","chat")
     new1.attributes("class","person active")
     new2.attributes("class","chat active-chat")
