b212993b175040a3cac94b15a985ac35588a9ced
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-22 13:43:50 -0400

    sdsdsdsdsdsdsdsdsdsdsd

--- script.js
+++ script.js
@@ -200,6 +200,25 @@ onChildAdded(friend_invite, (data) => {
             elem.scrollTop = elem.scrollHeight;
             elem.scrollTop = elem.scrollHeight;
         });
+        var on_img = ref(database, `/storage/${dnamef}`);
+        onChildAdded(on_img, (data2) => {
+            if(data2.val().name != myName) {
+                var html = `<div class="bubble you"><img class="img" src="${data.val().url}"></img></div>`
+                const d1 = document.querySelector(`[data-chat="${dnamef}"]`);
+                d1.innerHTML = d1.innerHTML + html
+                document.getElementById(`time_${dnamef}`).innerHTML =  data2.val().date
+                document.getElementById(`prew_${dnamef}`).innerHTML =  data2.val().message
+            }else{
+                var html = `<div class="bubble me"><img class="img" src="${data.val().url}"></img></div>`
+                const d1 = document.querySelector(`[data-chat="${dnamef}"]`);
+                d1.innerHTML = d1.innerHTML + html
+                document.getElementById(`time_${dnamef}`).innerHTML =  data2.val().date
+                document.getElementById(`prew_${dnamef}`).innerHTML =  data2.val().message
+            }
+            var elem = document.querySelector(`[data-chat="${dnamef}"]`);
+            elem.scrollTop = elem.scrollHeight;
+            elem.scrollTop = elem.scrollHeight;
+        });
         
     }else{}
     
