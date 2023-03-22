03cf16073fde02d41422116d2ea391829feb05d4
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-22 19:30:01 -0400

    dsdsdsdsdsds

--- file.js
+++ file.js
@@ -1,6 +1,21 @@
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
 
+const storage = getStorage();
+const storageRef = ref(storage, 'some-child');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+    authDomain: "ic-hat.firebaseapp.com",
+    databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+    projectId: "ic-hat",
+    storageBucket: "ic-hat.appspot.com",
+    messagingSenderId: "720687529085",
+    appId: "1:720687529085:web:2d964e880c5e2398058514",
+    measurementId: "G-YC8K0D7GLR"
+  };
+  const app = initializeApp(firebaseConfig);
+  
 // Get the modal
 var modal = document.getElementById("file");
 
