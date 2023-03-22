b61a8e80a61cc8b808e016e878b12418e9a9d293
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-22 17:02:44 -0400

    sdsdsdd

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 
-import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.17.2/storage";
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-storage";
 
 // Get the modal
 var modal = document.getElementById("file");
