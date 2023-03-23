01ac236d9dfa2b7062a6befbd7380049feb980d2
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-23 09:07:48 -0400

    sddddddddddddddd

--- core.js
+++ core.js
@@ -90,6 +90,7 @@ friends.addEventListener('click', (e) => {
 
 send.addEventListener('click', (e) => {
     var fg = document.getElementById('content').value
+    console.log(fg)
     var gh = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     if (document.getElementById('content').value != "" && fg.search(gh) != -1) {
         var str = document.getElementById('content').value;
@@ -113,6 +114,7 @@ send.addEventListener('click', (e) => {
 send2.addEventListener("keydown", (e) => {
     if (event.keyCode == 13) {
         var fg = document.getElementById('content').value
+        console.log(fg)
         var gh = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
         if (document.getElementById('content').value != "" && fg.search(gh) != -1) {
             var str = document.getElementById('content').value;
