dce28c27295ff68494479fcabfacc5157083c40b
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-23 08:59:06 -0400

    sdsdsdsdsd

--- core.js
+++ core.js
@@ -91,7 +91,7 @@ friends.addEventListener('click', (e) => {
 send.addEventListener('click', (e) => {
     var fg = document.getElementById('content').value
     var gh = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    if (document.getElementById('content').value != "" && fg.find(gh) != -1) {
+    if (document.getElementById('content').value != "" && fg.search(gh) != -1) {
         var str = document.getElementById('content').value;
         var str1 = str.replaceAll("<","&lt;")
         var str2 = str1.replaceAll(">","&gt;")
@@ -114,7 +114,7 @@ send2.addEventListener("keydown", (e) => {
     if (event.keyCode == 13) {
         var fg = document.getElementById('content').value
         var gh = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        if (document.getElementById('content').value != "" && fg.find(gh) != -1) {
+        if (document.getElementById('content').value != "" && fg.search(gh) != -1) {
             var str = document.getElementById('content').value;
             var str1 = str.replaceAll("<","&lt;")
             var str2 = str1.replaceAll(">","&gt;")
