ee1fe28bbbf94e8381eb4223dbb454e76c119631
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-24 19:50:49 -0400

    sdsdsdsdsdsdsd

--- file.js
+++ file.js
@@ -1,7 +1,9 @@
 add_file = document.getElementById("add_image")
 add_file.addEventListener('click', (e) => {
     const formData = new FormData();
+    
     const fileField = document.getElementById("file_input").files
+    data.append('file', fileField.files[0])
     var storageId = document.getElementsByClassName('active').id
     var url;
     fetch("https://fireimage.francoischoui1.repl.co/success", {
