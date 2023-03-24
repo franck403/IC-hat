7d24fc3321494ba22798a320be76d2722a12e711
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-24 13:56:27 -0400

    sdsdsdsdsdsds

--- file.js
+++ file.js
@@ -1,17 +1,18 @@
 
 add_file.addEventListener('click', (e) => {
+    const formData = new FormData();
+    const fileField = document.getElementById("file_input").files
     var storageId = document.getElementsByClassName('active').id
-    var imag = document.getElementById("file_input").files
-    const file = document.getElementById("file_input").files[0];
-    const name = +new Date() + `-${storageId}-` + file.name;
-    const metadata = {
-       contentType: file.type
-    };
     var url;
-    fetch("fireimage.francoischoui1.repl.co")
-    .then(snapshot => snapshot.ref2.getDownloadURL()) 
-    /*const task = ref2.child(name).put(file, metadata);task
-    .then(snapshot => snapshot.ref2.getDownloadURL()); */
+    fetch("https://fireimage.francoischoui1.repl.co/success", {
+        method: "POST",
+        mode:"no-cors",
+        body: formData,
+    })
+    .then((response) => url = response)
+    .catch((error) => {
+        console.error("Error:", error);
+    });
     modal.style.display = "none";
     document.getElementById("file_input").value = "";
 
