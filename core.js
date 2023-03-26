4a3c2fd60df7723fb1923fa54994abab441679c7
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-26 14:27:12 -0400

    sadasdsad

--- core.js
+++ core.js
@@ -202,7 +202,7 @@ add_file.addEventListener('click', (e) => {
         body: formData
     })
     .then((response) => {
-        var url = response.text
+        var url = response.text()
         console.log(url)
         var name = myName;
         const id = push(child(ref(database), 'messages')).key;
