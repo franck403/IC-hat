276cb988f75e44e881d27684dd68d14835a7ee32
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-03 17:42:14 -0400

    sdadsdasdsdsdasd

---
+++ login-register.js
@@ -0,0 +1,9 @@
+var login = document.getElementById("login")
+var register = document.getElementById("login")
+
+login.addEventListener('click', (e) => {
+    window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/login")
+});
+register.addEventListener('click', (e) => {
+    window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/login")
+});
