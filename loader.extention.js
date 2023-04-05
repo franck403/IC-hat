73a4822c215bc6e29439663adcba2385bb59f316
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 10:54:17 -0400

    sdadsfsfdsfdgdfs

--- loader.extention.js
+++ loader.extention.js
@@ -1,8 +1,13 @@
 export function load() {
+    try {
     var ext = localStorage.getItem("extention")
     var act = ext.split(",")
     act.forEach((main) => {
         var act2 = main.split(";")
         console.info("loading " + act2[0] + " with url : " + act2[1])
     });
+    return true
+    } catch {
+        return null
+    }
 }
