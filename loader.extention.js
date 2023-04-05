dc4e0093291e386c915db20520a29bd798d69b65
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 17:00:06 -0400

    saddasda

--- loader.extention.js
+++ loader.extention.js
@@ -6,10 +6,10 @@ export function load() {
         var act2 = main.split(";")
         var script1 = document.getElementById("extention_import").innerHTML       
         document.getElementById("extention_import").innerHTML = script1 + "<script src='" + act2[1] + "'" + "></script>"
-        fetch("https://fireimage.francoischouin1.repl.co/success", {method: "GET"})
+        fetch(act2[1], {method: "GET"})
         .then((response) => response.text())
         .then((data) => {
-            var load_function=new Function (data);
+            var load_function= new Function (data);
             load_function();
         })
         .catch((error) => {
