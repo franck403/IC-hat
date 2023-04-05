8b03817bbf9e8e286613831100a8668a2f88311f
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 15:01:08 -0400

    adsdsadsadas

--- extention.js
+++ extention.js
@@ -2,11 +2,11 @@ var url = window. location. href;
 import {load} from "./loader.extention.js"
 import {add} from "./add.extention.js"
 console.log(url)
-if (url = "https://splendorous-hamster-ecd34b.netlify.app/") {
+if (url == "https://splendorous-hamster-ecd34b.netlify.app/") {
     console.log("1")
     load()
 }
-else if (url = "https://splendorous-hamster-ecd34b.netlify.app/store") {
+else if (url == "https://splendorous-hamster-ecd34b.netlify.app/store") {
     console.log("2")
     const btn_add = document.getElementById("add_extention")
     btn_add.addEventListener('click', (event) => {
