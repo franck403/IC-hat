535f71a779ec21a4d9c2d89e1e345d5818d4ef99
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 06:28:55 -0400

    asdfgewretgdf

---
+++ home.js
@@ -0,0 +1,14 @@
+fetch("https://I-home.calebgauthier.repl.co", {
+    method: "POST",
+    body: formData
+})
+.then((response) => response.text())
+.then((data) => {
+    var ldata = data.replace("<!-- Ce fichier est pour les regles merci de ne pas toucher sauf pour les changer merci -->","")
+    document.getElementById("homedata").innerHTML(ldata)
+})
+.catch((error) => {
+    console.error("Error:", error);
+});
+
+
