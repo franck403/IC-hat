78072837e762a83e8a9528e6468feb81979cd26a
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 15:05:29 -0400

    sdadasdsdasdas

--- loader.extention.js
+++ loader.extention.js
@@ -5,6 +5,8 @@ export function load() {
     act.forEach((main) => {
         var act2 = main.split(";")
         console.info("loading " + act2[0] + " with url : " + act2[1])
+        var script1 = document.getElementById("extention_import").innerHTML
+        document.getElementById("extention_import").innerHTML = script1 + "<script src='" + act2[1] + "'" + "></script>"
     });
     return true
     } catch {
