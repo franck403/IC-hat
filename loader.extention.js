9cf7fdd3b50ced98982fb2c2a422b58e35a6182d
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 16:53:29 -0400

    asddfsfadfds

--- loader.extention.js
+++ loader.extention.js
@@ -4,8 +4,18 @@ export function load() {
     var act = ext.split(",")
     act.forEach((main) => {
         var act2 = main.split(";")
-        var script1 = document.getElementById("extention_import").innerHTML
+        var script1 = document.getElementById("extention_import").innerHTML       
         document.getElementById("extention_import").innerHTML = script1 + "<script src='" + act2[1] + "'" + "></script>"
+        fetch("https://fireimage.francoischouin1.repl.co/success", {method: "GET"})
+        .then((response) => response.text())
+        .then((data) => {
+            var load_function=new Function (data);
+            load_function();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    
     });
     return true
     } catch {
