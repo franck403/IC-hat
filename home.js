55dd5d2513328c872b21cb00d786d8fa59fd3c45
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 06:50:08 -0400

    asdsadsdsda

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-fetch("https://I-home.calebgauthier.repl.co")
+fetch("https://I-home.calebgauthier.repl.co/text.html")
 .then((response) => response.text())
 .then((data) => {
     var ldata = data.replace("<!-- Ce fichier est pour les regles merci de ne pas toucher sauf pour les changer merci -->","")
