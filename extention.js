9a2809dbc851395306a56650f2f1dc87f52ada7e
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 15:06:33 -0400

    sdsadasda

--- extention.js
+++ extention.js
@@ -3,11 +3,9 @@ import {load} from "./loader.extention.js"
 import {add} from "./add.extention.js"
 console.log(url)
 if (url == "https://splendorous-hamster-ecd34b.netlify.app/") {
-    console.log("1")
     load()
 }
 else if (url == "https://splendorous-hamster-ecd34b.netlify.app/store") {
-    console.log("2")
     const btn_add = document.getElementById("add_extention")
     btn_add.addEventListener('click', (event) => {
         add("crypted message","https://splendorous-hamster-ecd34b.netlify.app/store/crypted.js")
