857997345d42f3c9ff12c0ed0335f51d7d8ba92c
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 19:56:08 -0400

    asdfadfsafsg

--- loader.extention.js
+++ loader.extention.js
@@ -3,20 +3,22 @@ export function load() {
     var ext = localStorage.getItem("extention")
     var act = ext.split(",")
     act.forEach((main) => {
-        var act2 = main.split(";")
-        fetch(act2[1], {method: "GET"})
-        .then((response) => response.text())
-        .then((data) => {
-            var tag = document.createElement("script");
-            tag.src = act2[1];
-            tag.type = "module" 
-            document.getElementById("extention_import").appendChild(tag);
-            tag.setAttribute("type","module")
-            console.log("[extention loader] " + act2[1] + " loaded")
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
+        if (main != null) {
+            var act2 = main.split(";")
+            fetch(act2[1], {method: "GET"})
+            .then((response) => response.text())
+            .then((data) => {
+                var tag = document.createElement("script");
+                tag.src = act2[1];
+                tag.type = "module" 
+                document.getElementById("extention_import").appendChild(tag);
+                tag.setAttribute("type","module")
+                console.log("[extention loader] " + act2[1] + " loaded")
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+        } else{}
     
     });
     return true
