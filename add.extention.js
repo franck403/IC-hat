66eab961ad9e7c38eb470f779d92527f1ff8cdbd
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 10:55:36 -0400

    ssdfdfgdfs

--- add.extention.js
+++ add.extention.js
@@ -1,7 +1,7 @@
-if (localStorage.getItem("extention") != null) {} else {    
-    localStorage.setItem("extention","")    
-}
+if (localStorage.getItem("extention") != null) {} else {localStorage.setItem("extention","")}
+
 export function add(name,url) {
     var old = localStorage.getItem("extention")
     localStorage.setItem("extention", old + "," + name + ";" + url)
+    window.location.reload("https://splendorous-hamster-ecd34b.netlify.app")
 }
