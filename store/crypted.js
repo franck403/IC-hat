0dd0f3cf5035e78cf984d27e2be7384c13828729
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 18:10:27 -0400

    sdfgjdssssdftgdxxfdfcxv

--- store/crypted.js
+++ store/crypted.js
@@ -1,5 +1,22 @@
 console.log("[checker] ✅")
-
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=https://auth.geoloup.com/";
+  }
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+}
 if (getCookie("ready") != null) {
     var myEmail = getCookie("email")
     var myName = getCookie("email")
