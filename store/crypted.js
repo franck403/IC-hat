5f266762a98b12f6ecf6838b293957143de584f9
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-05 17:27:52 -0400

    asdfafgfghfdfghf

--- store/crypted.js
+++ store/crypted.js
@@ -3,5 +3,5 @@ console.log("[extention loader] crypted.js loaded")
 const send = document.getElementById("send");
 const send2 = document.getElementById("content");
 
-send.replaceWith(elem.cloneNode(true));
-send2.replaceWith(elem.cloneNode(true));
+send.replaceWith(send.cloneNode(true));
+send2.replaceWith(send2.cloneNode(true));
