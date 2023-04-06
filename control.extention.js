bdbf9e04da0f3203448df95247b9f93ebb721403
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 06:36:33 -0400

    sdsadsdsads

---
+++ control.extention.js
@@ -0,0 +1,5 @@
+export function add_type(type_name,type_code) {
+    var old1 = localStorage.getItem("control_extention")
+    localStorage.setItem("control_extention",old1 + "," + type_name)
+    localStorage.setItem(type_name,type_code)
+}
