654362b82cda69fc653114c34cd93d132b83ff79
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 08:00:08 -0400

    sdsadasdas

--- store/build.js
+++ store/build.js
@@ -1,5 +1,7 @@
 var codeEditor = document.getElementById('codeEditor');
 var lineCounter = document.getElementById('lineCounter');
+var codeEditor = document.getElementById('codeEditor2');
+var lineCounter = document.getElementById('lineCounter2');
 
 codeEditor.addEventListener('scroll', () => {
     lineCounter.scrollTop = codeEditor.scrollTop;
@@ -11,7 +13,7 @@ codeEditor.addEventListener('keydown', (e) => {
 if (keyCode === 9) {  // TAB = 9
          e.preventDefault();
          codeEditor.value = value.slice(0, selectionStart) + '\t' + value.slice(selectionEnd);
-         codeEditor.setSelectionRange(selectionStart+2, selectionStart+2)
+         codeEditor.setSelectionRange(selectionStart+1, selectionStart+1)
        }
  });
 
@@ -28,5 +30,20 @@ function line_counter() {
       lineCountCache = lineCount;
 }
 codeEditor.addEventListener('input', () => {
+    line_counter();
+});
+var lineCountCache2 = 0;
+function line_counter2() {
+      var lineCount2 = codeEditor2.value.split('\n').length;
+      var outarr = new Array();
+      if (lineCountCache2 != lineCount2) {
+         for (var x = 0; x < lineCount2; x++) {
+            outarr[x] = (x + 1) + '.';
+         }
+         lineCounter2.value = outarr.join('\n');
+      }
+      lineCountCache2 = lineCount2;
+}
+codeEditor2.addEventListener('input', () => {
     line_counter();
 });
