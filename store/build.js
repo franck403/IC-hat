42a9f3007621091e3dd8c63715e137fc1a4bb0b9
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 06:47:37 -0400

    sdasdads

---
+++ store/build.js
@@ -0,0 +1,32 @@
+var codeEditor = document.getElementById('codeEditor');
+var lineCounter = document.getElementById('lineCounter');
+
+codeEditor.addEventListener('scroll', () => {
+    lineCounter.scrollTop = codeEditor.scrollTop;
+    lineCounter.scrollLeft = codeEditor.scrollLeft;
+});
+codeEditor.addEventListener('keydown', (e) => {
+       let { keyCode } = e;
+       let { value, selectionStart, selectionEnd } = codeEditor;
+if (keyCode === 9) {  // TAB = 9
+         e.preventDefault();
+         codeEditor.value = value.slice(0, selectionStart) + '\t' + value.slice(selectionEnd);
+         codeEditor.setSelectionRange(selectionStart+2, selectionStart+2)
+       }
+ });
+
+var lineCountCache = 0;
+function line_counter() {
+      var lineCount = codeEditor.value.split('\n').length;
+      var outarr = new Array();
+      if (lineCountCache != lineCount) {
+         for (var x = 0; x < lineCount; x++) {
+            outarr[x] = (x + 1) + '.';
+         }
+         lineCounter.value = outarr.join('\n');
+      }
+      lineCountCache = lineCount;
+}
+codeEditor.addEventListener('input', () => {
+    line_counter();
+});
