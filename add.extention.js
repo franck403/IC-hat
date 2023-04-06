e998ea2ed02fff82c5d8a2480bb1dda1d9f892b2
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 12:34:03 -0400

    rbhyu

--- add.extention.js
+++ add.extention.js
@@ -1,16 +1,9 @@
 if (localStorage.getItem("extention") != null) {} else {localStorage.setItem("extention","")}
 
 export function add(name,url) {
-    if (document.querySelectorAll("[data-name='" + act2[0] +"']")[0] == null) {
-        var old = localStorage.getItem("extention")
-        localStorage.setItem("extention", old +  "," + name + ";" + url)
-        window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/")
-    } else {
-        var old = localStorage.getItem("extention")
-        var old = old.replace("," + name + ";" + url,"")
-        localStorage.setItem("extention", old +  "," + name + ";" + url)
-        window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/")
-    }
+    var old = localStorage.getItem("extention")
+    localStorage.setItem("extention", old +  "," + name + ";" + url)
+    window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/")
 }
 
 export function check() {
