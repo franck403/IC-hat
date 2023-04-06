fec7c877da268340c42177c04fe9d93764910fc0
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 11:01:18 -0400

    sdsadsadasd

--- add.extention.js
+++ add.extention.js
@@ -5,3 +5,18 @@ export function add(name,url) {
     localStorage.setItem("extention", old +  "," + name + ";" + url)
     window.location.replace("https://splendorous-hamster-ecd34b.netlify.app/")
 }
+
+export function check() {
+    var ext = localStorage.getItem("extention")
+    var act = ext.split(",")
+    act.forEach((main) => {
+        if (main != "") {
+            var act2 = main.split(";")
+            var act3 = document.querySelectorAll("[data-name='" + act2[0] +"']")[0]
+            act3.setAttribute("class","store-1 store_box store-added")
+            act3.setAttribute("disabled","true")
+
+        } else{}
+
+    });
+}
--- extention.js
+++ extention.js
@@ -6,6 +6,7 @@ if (url == "https://splendorous-hamster-ecd34b.netlify.app/") {
     load()
 }
 else if (url == "https://splendorous-hamster-ecd34b.netlify.app/store") {
+    if (localStorage.getItem("extention") != null) {} else {localStorage.setItem("extention","")}
     const btn_add = document.getElementById("add_extention")
     btn_add.addEventListener('click', (event) => {
         add("crypted message","https://splendorous-hamster-ecd34b.netlify.app/store/crypted.js")
