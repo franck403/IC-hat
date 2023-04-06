e35ccfdbe0c61f3a164b2c279a27efc19be5f711
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-06 08:02:00 -0400

    sadsadsadasdadsasda

--- store/build.js
+++ store/build.js
@@ -1,7 +1,5 @@
 var codeEditor = document.getElementById('codeEditor');
 var lineCounter = document.getElementById('lineCounter');
-var codeEditor = document.getElementById('codeEditor2');
-var lineCounter = document.getElementById('lineCounter2');
 
 codeEditor.addEventListener('scroll', () => {
     lineCounter.scrollTop = codeEditor.scrollTop;
@@ -30,20 +28,5 @@ function line_counter() {
       lineCountCache = lineCount;
 }
 codeEditor.addEventListener('input', () => {
-    line_counter();
-});
-var lineCountCache2 = 0;
-function line_counter2() {
-      var lineCount2 = codeEditor2.value.split('\n').length;
-      var outarr = new Array();
-      if (lineCountCache2 != lineCount2) {
-         for (var x = 0; x < lineCount2; x++) {
-            outarr[x] = (x + 1) + '.';
-         }
-         lineCounter2.value = outarr.join('\n');
-      }
-      lineCountCache2 = lineCount2;
-}
-codeEditor2.addEventListener('input', () => {
     line_counter();
 });
