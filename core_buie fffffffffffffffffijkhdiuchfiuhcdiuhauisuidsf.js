1be259e017af021cbfc24205f8e2892623604b96
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-07 13:21:52 -0400

    sadsdsadas

--- core_buie fffffffffffffffffijkhdiuchfiuhcdiuhauisuidsf.js
+++ core_buie fffffffffffffffffijkhdiuchfiuhcdiuhauisuidsf.js
@@ -340,7 +340,7 @@ onChildAdded(friend_invite, (data) => {
                     */
                     var stim = localStorage.getItem("wait")
                     setTimeout(cryptmess(data2.val().message,data2),1000 + stim)
-                    localStorage.setItem("wait",stim + 500)
+                    localStorage.setItem("wait",stim + 1000)
                 } else {}
             }
             else {}
