2171d938fa9b43fc8e43df1c3f897310f5e4a766
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-10 17:41:58 -0400

    asdsdsadas

--- normal/message.secret.code.js
+++ normal/message.secret.code.js
@@ -34,7 +34,6 @@ export function SendMessage(ext_name,ext_type,content) {
     var name = myName;
     const id = push(child(ref(database), 'custom/extention/' + ext_name)).key;
     var cusid = document.getElementsByClassName('person active')[0].id
-    
     set(ref(database, 'custom/message/'+ id), {
         email:name,
         type:ext_type,
