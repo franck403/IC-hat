8fd3e6d9c83f61b214e744869f23331a9ea500f2
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-09 21:08:25 -0400

    ads

--- devkit.extention.js
+++ devkit.extention.js
@@ -14,6 +14,7 @@ export function OnMessage(val) {
         var CodeToLauchListNotSplit = localStorage.getItem("devkit")
         var CodeToLauchList = CodeToLauchListNotSplit.split("|__|")
         CodeToLauchList.forEach(function(name){
+            console.log(name)
             if (name != "") {
                 var CodeToLauch = localStorage.getItem("devkit_" + name)
                 LauchCode(CodeToLauch)
