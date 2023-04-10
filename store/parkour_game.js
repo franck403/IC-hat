2f5d74ac51b8ff3a0bf715851ab898ddd1c44e57
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-10 17:13:48 -0400

    asdasdsad

--- store/parkour_game.js
+++ store/parkour_game.js
@@ -1,8 +1,8 @@
 import {SendMessage} from 'https://splendorous-hamster-ecd34b.netlify.app/message.extention.js'
 import {OnNewMessage} from 'https://splendorous-hamster-ecd34b.netlify.app/devkit.extention.js'
 
-SendMessage("devkit_test","devkit_test","TEST")
-
 OnNewMessage.setup_code("consolelog",(data) => {
     console.log("type : " + data.type + " message : " + data.message)
 })
+
+SendMessage("devkit_test","devkit_test","TEST")
