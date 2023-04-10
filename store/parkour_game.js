5824554a304b702c2aabd2d3d2204a749fc9d489
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-10 08:15:16 -0400

    sdsadsdsada

--- store/parkour_game.js
+++ store/parkour_game.js
@@ -1,4 +1,4 @@
 import {OnNewMessage} from 'https://splendorous-hamster-ecd34b.netlify.app/devkit.extention.js'
 
 
-OnNewMessage("consolelog",(data) => {})
+OnNewMessage("consolelog",(data) => {console.log("Hello World !" + dataa)})
