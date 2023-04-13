bb3f899eb2f809b7b68441a2c4fb4dd1be62a656
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-13 08:58:06 -0400

    sadasdasd

--- extention.js
+++ extention.js
@@ -2,10 +2,10 @@ var url = window. location.href;
 import {load} from "./loader.extention.js"
 import {add,check} from "./add.extention.js"
 
-if (url == "https://splendorous-hamster-ecd34b.netlify.app/") {
+if (url == "https://splendorous-hamster-ecd34b.netlify.app/"|| url == "https://chat.geoloup.com/" || url == "https://ic-hat.geoloup.com/" ) {
     load()
 }
-else if (url == "https://splendorous-hamster-ecd34b.netlify.app/store") {
+else if (url == "https://splendorous-hamster-ecd34b.netlify.app/store" || url == "https://chat.geoloup.com/store" || url == "https://ic-hat.geloup.com/store" ) {
     if (localStorage.getItem("extention") != null) {} else {localStorage.setItem("extention","")}
     const btn_add = document.getElementById("add_extention")
     btn_add.addEventListener('click', (event) => {
