23af73e3cfcf50ab238e04ac84f185e2136bc200
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-21 16:11:06 -0400

    addsada

--- bhuy3huygyufwyuge.js
+++ bhuy3huygyufwyuge.js
@@ -85,6 +85,7 @@ export function register(email,password) {
 }
 
 export function getuser() {
+  if (getCookie("geoloup") != null) {
   return fetch("https://auth.francoischouin1.repl.co/getuser?geoloup=" + getCookie("geoloup"))
   .then((reponse) => reponse.text())
   .then((data) => {
@@ -96,5 +97,7 @@ export function getuser() {
   })
   .catch(() => {
     return null
-  });
+  })} else {
+    return null
+  }
 }
