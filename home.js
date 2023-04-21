40fdd17bd33343ad4f1819b0605e7eb547b83811
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-21 17:01:52 -0400

    asda

--- home.js
+++ home.js
@@ -4,6 +4,4 @@ fetch("https://I-home.calebgauthier.repl.co/text.html")
     var ldata = data.replace("<!-- Ce fichier est pour les regles merci de ne pas toucher sauf pour les changer merci -->","")
     document.getElementById("homedata").innerHTML = ldata
 })
-.catch((error) => {
-    console.error("Error:", error);
-});
+.catch((error) => {});
