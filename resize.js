8b8216d2397ccb02b093017447cc92049048ebad
franck403 <79372051+franck403@users.noreply.github.com>
2023-04-23 19:58:10 -0400

    sdad

---
+++ resize.js
@@ -0,0 +1,15 @@
+function resize(){
+
+    var actual_size = window.innerWidth
+    var max_size = 971
+    if (actual_size < max_size) {    
+        var old1 = document.getElementsByClassName("write write-active")[0].setAttribute("class","write")
+        var old2 = document.getElementsByClassName("chat active-chat")[0].setAttribute("class","chat")
+        var old3 = document.getElementsByClassName("top")[1].setAttribute("class","top")
+
+    } else {
+        var old1 = document.getElementsByClassName("write write-active")[0].setAttribute("class","write")
+        var old2 = document.getElementsByClassName("chat active-chat")[0].setAttribute("class","chat")
+        var old3 = document.getElementsByClassName("top")[1].setAttribute("class","top")    
+    }
+}
