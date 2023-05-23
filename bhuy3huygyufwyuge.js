c1ade5adb53c9d1c0fc9bfa3f4a3f6eba6513066
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-23 19:24:13 -0400

    sadsaa

--- bhuy3huygyufwyuge.js
+++ bhuy3huygyufwyuge.js
@@ -159,9 +159,11 @@ export function message_render(mesage) {
 	var unzalgo = function () {
 		message_start.value = ((message_start.value || "").match(r) || [""]).join("");
 	};
+  var message_start = unzalgo
   var r = /[\u0000-\u02FF\u0370–\uFFFF]/g;
 	var unzalgo = function () {
 		message_start.value = ((message_start.value || "").match(r) || [""]).join("");
 	};
+  var message_start = unzalgo
   return message_start
 }
