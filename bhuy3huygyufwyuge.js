64564a624d1fffa9a026d0b03674fdbfb80469b1
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-25 19:25:21 -0400

    adsda

--- bhuy3huygyufwyuge.js
+++ bhuy3huygyufwyuge.js
@@ -176,6 +176,31 @@ export function message_render(message) {
 }
 
 export function image_render(email,name) {
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  import {
+      getDatabase,
+      set,
+      ref,
+      push,
+      child,
+      onValue,
+      onChildAdded
+  } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+    authDomain: "ic-hat.firebaseapp.com",
+    databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+    projectId: "ic-hat",
+    storageBucket: "ic-hat.appspot.com",
+    messagingSenderId: "720687529085",
+    appId: "1:720687529085:web:2d964e880c5e2398058514",
+    measurementId: "G-YC8K0D7GLR"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
   var name = email;
   var myName = name
   var filelist = document.getElementById("file_input").files
