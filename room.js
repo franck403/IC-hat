e65da25e9f7e10ca16e89440704f60f7ddd275c0
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-25 10:46:05 -0400

    dfsdfdsfdfsd

--- room.js
+++ room.js
@@ -48,3 +48,27 @@ function mobile() {
     var old4 = document.getElementsByClassName("mobile mobile-active")[0].setAttribute("class","mobile")
     var old5 = document.getElementsByClassName("mobile-frame mobile-frame-active")[0].setAttribute("class","mobile-frame")
 }
+
+function dropHandler(ev) {
+    console.log("File(s) dropped");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+        }
+      });
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+      });
+    }
+  }
+  
