41b99613a11189d945bd3e28528d8ead8573ea48
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-27 16:29:12 -0400

    sadas

--- room.js
+++ room.js
@@ -3,7 +3,6 @@ function room(id){
         var new2 = document.getElementById(id + "_c")
     } else if (id.search("new") != -1) {
       var research = id.replace("new","")
-      console.log(research)
       document.getElementById("friend_email").value = research
       document.getElementById("new_friend_add").click()
       document.getElementById('search_bar').value = ""
