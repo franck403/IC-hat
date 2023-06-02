9603473c6e7a86966f60704a2b6b87a7a02432ce
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-02 06:47:31 -0400

    sdsd

--- profile.js
+++ profile.js
@@ -31,7 +31,7 @@ c.addEventListener("click", () => {
             var img = "png;base64," + btoa(reader.result)
             changepic(img,myEmail)
         }
-        reader.readAsBinaryString(inout.files[0]);    
+        reader.readAsBinaryString(input.files[0]);    
     };
     input.click();
 });
