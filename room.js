f3070aaa254e77ec38d1a1c855d6367dffb74f11
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-19 18:54:15 -0400

    add new thing

--- room.js
+++ room.js
@@ -1,53 +1,53 @@
-function room(id){
-    if (id == "geoloup_chat") {
-        var new2 = document.getElementById(id + "_c")
-    } else if (id.search("new") != -1) {
-      var research = id.replace("new","")
-      document.getElementById("friend_email").value = research
-      document.getElementById("new_friend_add").click()
-      document.getElementById('search_bar').value = ""
-      search()
-      var new2 = document.getElementById("room_" + research)
-    } else {
-      var new2 = document.getElementById("room_" + id)
-    }
-    var old1 = document.getElementsByClassName("write")[0].setAttribute("class","write write-active")
-    var old2 = document.getElementsByClassName("chat active-chat")[0]
-    var old4 = document.getElementsByClassName("mobile")[0].setAttribute("class","mobile mobile-active")
-    var old5 = document.getElementsByClassName("mobile-frame")[0].setAttribute("class","mobile-frame mobile-frame-active")
-    try {
-      var old6 = document.getElementsByClassName("left left-active")[0].setAttribute("class","left")
-      var old7 = document.getElementsByClassName("top")[1].setAttribute("class","top top-active")
-    } catch {
+function room(id) {
+  if (id == "geoloup_chat") {
+    var new2 = document.getElementById(id + "_c")
+  } else if (id.search("new") != -1) {
+    var research = id.replace("new", "")
+    document.getElementById("friend_email").value = research
+    document.getElementById("new_friend_add").click()
+    document.getElementById('search_bar').value = ""
+    search()
+    var new2 = document.getElementById("room_" + research)
+  } else {
+    var new2 = document.getElementById("room_" + id)
+  }
+  var old1 = document.getElementsByClassName("write")[0].setAttribute("class", "write write-active")
+  var old2 = document.getElementsByClassName("chat active-chat")[0]
+  var old4 = document.getElementsByClassName("mobile")[0].setAttribute("class", "mobile mobile-active")
+  var old5 = document.getElementsByClassName("mobile-frame")[0].setAttribute("class", "mobile-frame mobile-frame-active")
+  try {
+    var old6 = document.getElementsByClassName("left left-active")[0].setAttribute("class", "left")
+    var old7 = document.getElementsByClassName("top")[1].setAttribute("class", "top top-active")
+  } catch {
 
-    }
-    var new1 = document.getElementById(id)
-    var new1 = document.getElementById(id)
-    var to = document.querySelector("#to")
-    try {
-        old2.setAttribute("class","chat")
-    } catch {}
-    new2.setAttribute("class","chat active-chat")
-    to.innerHTML = new1.dataset.name
-    new2.scrollTop = new2.scrollHeight;
-    new2.scrollTop = new2.scrollHeight; 
+  }
+  var new1 = document.getElementById(id)
+  var new1 = document.getElementById(id)
+  var to = document.querySelector("#to")
+  try {
+    old2.setAttribute("class", "chat")
+  } catch { }
+  new2.setAttribute("class", "chat active-chat")
+  to.innerHTML = new1.dataset.name
+  new2.scrollTop = new2.scrollHeight;
+  new2.scrollTop = new2.scrollHeight;
 }
 // spam counter
 var send_by_img = document.getElementById("send")
 var send_by_enter = document.getElementById("content")
 // spam database
 var spam_database = {
-    spam_counter : 0,
-    spam_max : 5
+  spam_counter: 0,
+  spam_max: 5
 }
 // spam code
 let spam_event = (spam_database) => {
-    spam_database.spam_counter = spam_database.spam_counter + 1
-    if (spam_database == spam_database.spam_counter) {
-        document.getElementById("content").setAttribute("disable","true")
-    } else {
-        document.getElementById("content").removeAttribute("disable")
-    }
+  spam_database.spam_counter = spam_database.spam_counter + 1
+  if (spam_database == spam_database.spam_counter) {
+    document.getElementById("content").setAttribute("disable", "true")
+  } else {
+    document.getElementById("content").removeAttribute("disable")
+  }
 }
 // spam listener
 send_by_img.addEventListener('click', spam_event(spam_database));
@@ -55,73 +55,73 @@ send_by_enter.addEventListener('click', spam_event(spam_database));
 
 
 function mobile() {
-  var old1 = document.getElementsByClassName("write write-active")[0].setAttribute("class","write")
-  var old2 = document.getElementsByClassName("chat active-chat")[0].setAttribute("class","chat")
-  var old4 = document.getElementsByClassName("mobile mobile-active")[0].setAttribute("class","mobile")
-  var old5 = document.getElementsByClassName("mobile-frame mobile-frame-active")[0].setAttribute("class","mobile-frame")
-  var old6 = document.getElementsByClassName("left")[0].setAttribute("class","left left-active")
+  var old1 = document.getElementsByClassName("write write-active")[0].setAttribute("class", "write")
+  var old2 = document.getElementsByClassName("chat active-chat")[0].setAttribute("class", "chat")
+  var old4 = document.getElementsByClassName("mobile mobile-active")[0].setAttribute("class", "mobile")
+  var old5 = document.getElementsByClassName("mobile-frame mobile-frame-active")[0].setAttribute("class", "mobile-frame")
+  var old6 = document.getElementsByClassName("left")[0].setAttribute("class", "left left-active")
   try {
-    var old7 = document.getElementsByClassName("top top-active")[1].setAttribute("class","top")
+    var old7 = document.getElementsByClassName("top top-active")[1].setAttribute("class", "top")
   } catch {
-    
+
   }
 }
 
 function dropHandler(ev) {
-    console.log("File(s) dropped");
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        if (item.kind === "file") {
-          console.log(ev.dataTransfer.files)
-            var imageTypes = ['image/png', 'image/gif', 'image/bmp', 'image/jpeg'];
-            if (imageTypes.includes(item.type) != 0) {
-              const file = item.getAsFile();
-              document.getElementById("file_input").files = ev.dataTransfer.files;
-              console.log(`${item.type}… file[${i}].name = ${file.name}`);
-            }
-            else {
-              var d  = ev.dataTransfer.files
-              console.log(d)
-            }
+  console.log("File(s) dropped");
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      if (item.kind === "file") {
+        console.log(ev.dataTransfer.files)
+        var imageTypes = ['image/png', 'image/gif', 'image/bmp', 'image/jpeg'];
+        if (imageTypes.includes(item.type) != 0) {
+          const file = item.getAsFile();
+          document.getElementById("file_input").files = ev.dataTransfer.files;
+          console.log(`${item.type}… file[${i}].name = ${file.name}`);
         }
-      });
-    } else {
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        console.log(`… file[${i}].name = ${file.name}`);
-      });
-    }
+        else {
+          var d = ev.dataTransfer.files
+          console.log(d)
+        }
+      }
+    });
+  } else {
+    [...ev.dataTransfer.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
 }
 
 function search() {
-	let input = document.getElementById('search_bar').value
-	input=input.toLowerCase();
-	let x = document.getElementsByClassName('people-person');
-	
-	for (i = 0; i < x.length; i++) {
+  let input = document.getElementById('search_bar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('people-person');
+
+  for (i = 0; i < x.length; i++) {
     if (!x[i].classList.contains("db")) {
       if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].style.display="none";
+        x[i].style.display = "none";
       }
       else {
-        x[i].style.display="list-item";				
+        x[i].style.display = "list-item";
       }
     }
-	}
+  }
   let x_test = 0
   let x_need = Object.keys(x).length
   for (i = 0; i < x.length; i++) {
-		if (x[i].style.display == "none") {
-			x_test++
-		}
+    if (x[i].style.display == "none") {
+      x_test++
+    }
     if (x[i].classList.contains("db")) {
       x[i].remove()
-			x_test++
+      x_test++
     }
-	}
+  }
   if (x_test == x_need) {
     let z = document.getElementsByClassName('people-person db');
     for (i = 0; i < z.length; i++) {
@@ -130,11 +130,11 @@ function search() {
     console.log("[search core] Searching Person in db...")
     document.getElementById('search_bar').disabled
     fetch('https://auth.francoischouin1.repl.co/user/search?name=' + input)
-      .then((response)=>response.json())
-      .then((responseJson)=>{
+      .then((response) => response.json())
+      .then((responseJson) => {
         for (i = 0; i < z.length; i++) {
           z[i].remove()
-        }    
+        }
         document.getElementById('search_bar').removeAttribute("disable")
         var list = responseJson
         Object.keys(list).forEach(key => {
@@ -172,4 +172,34 @@ function small() {
   var image = ""
   document.getElementById("big-img").src = image
   document.getElementById("big-img").style.display = "none"
+}
+
+function key_add() {
+  console.log("[core] Getting db content")
+  document.getElementById('friend_email').disabled
+  if (document.getElementById("firend_list_preview").innerHTML == "") {
+    fetch('https://auth.francoischouin1.repl.co/user/search?name=' + input)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        document.getElementById('friend_email').removeAttribute("disable")
+        var list = responseJson
+        Object.keys(list).forEach(key => {
+          var db = list[key]
+          var html = `
+          <li onclick="friend('new${db['email']}')" id="friend-preview-${db['email']}" class="friend-preview list">
+          <img src="img/default.png" class="people-img"alt="picture" />
+          <p id="name_" class="people-name">${db['name']}</p>
+          </li>`
+          const d2 = document.getElementById("firend_list_preview")
+          d2.innerHTML = d2.innerHTML + html
+        });
+      });
+  }
+}
+function friend(email) {
+  if (!document.getElementById("friend-preview-" + email).classList.contains("check")) {
+    document.getElementById("friend-preview-" + email).classList.toggle('check');
+  } else {
+    document.getElementById("friend-preview-" + email).classList.toggle('check');
+  }
 }
