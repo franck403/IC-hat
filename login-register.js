5f4166980a55b27528c0fc511fa686480be57e90
franck403 <79372051+franck403@users.noreply.github.com>
2023-08-31 18:59:30 -0400

    updatinh the login-register

--- login-register.js
+++ login-register.js
@@ -4,13 +4,28 @@ iframe.onload = () => {
     var fc = (if1.contentWindow || if1.contentDocument);
     var login = fc.document.getElementById("login");
     var register = fc.document.getElementById("register");
+    var start = fc.document.getElementById("start");
+    var log_out = fc.document.getElementById("out");
     var home = netlifyIdentity.currentUser()
-    if (home != null) {}
-
-    login.addEventListener('click', (e) => {
-        netlifyIdentity.open("login") 
-    });
-    register.addEventListener('click', (e) => {
-        netlifyIdentity.open("signup")
-    });
+    if (home != null) {
+        start.addEventListener('click', (e) => {
+            if (window.location.origin.endsWith("/")) {
+                window.location.replace(`${window.location.origin}chat`)
+            } else {
+                window.location.replace(`${window.location.origin}/chat`)
+            }
+        });
+        log_out.addEventListener('click', (e) => {
+            netlifyIdentity.logout()
+        });
+        document.getElementById("no").remove()
+    } else {
+        login.addEventListener('click', (e) => {
+            netlifyIdentity.open("login")
+        });
+        register.addEventListener('click', (e) => {
+            netlifyIdentity.open("signup")
+        });
+        document.getElementById("yes").remove()
+    }
 }
