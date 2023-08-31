c28ac3277ef8f1be9d12366208643340b2fe79a5
franck403 <79372051+franck403@users.noreply.github.com>
2023-08-31 12:02:03 -0400

    remove bug

--- core.js
+++ core.js
@@ -248,7 +248,7 @@ form.addEventListener('submit', (event) => {
 const friend_invite = ref(database, 'users_friend/');
 onChildAdded(friend_invite, (data) => {
     var dte = data.val().allow
-    if(dte.indexOf(myEmail) != -1 && "" in dte.split(",") && " " in dte.split(",")) {
+    if(dte.indexOf(myEmail) != -1) {
         var n_allow = data.val().allow
         try {
             var new_allow = n_allow.join(",")
