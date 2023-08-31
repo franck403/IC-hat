d66dced774effdad23a1978a7ac149c66088929c
franck403 <79372051+franck403@users.noreply.github.com>
2023-08-31 12:10:24 -0400

    add check for none

--- core.js
+++ core.js
@@ -248,7 +248,7 @@ form.addEventListener('submit', (event) => {
 const friend_invite = ref(database, 'users_friend/');
 onChildAdded(friend_invite, (data) => {
     var dte = data.val().allow
-    if(dte.indexOf(myEmail) != -1) {
+    if(dte.indexOf(myEmail) != -1 && dte == `,${myEmail}`) {
         var n_allow = data.val().allow
         try {
             var new_allow = n_allow.join(",")
