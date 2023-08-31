81914928a179ffc3bbb2e4c57ee5e24d4f18affb
franck403 <79372051+franck403@users.noreply.github.com>
2023-08-31 12:32:55 -0400

    reform

--- loader.extention.js
+++ loader.extention.js
@@ -1,31 +1,31 @@
-import {getCookie,getuser} from "./bhuy3huygyufwyuge.js"
+import { getCookie, getuser } from "./bhuy3huygyufwyuge.js"
 export function load() {
     try {
-    var ext = localStorage.getItem("extention")
-    var act = ext.split(",")
-    act.forEach((main) => {
-        if (main != "" && getCookie("geoloup") != null) {
-            var act2 = main.split(";")
-            fetch(act2[1], {method: "GET"})
-            .then((response) => response.text())
-            .then((data) => {
-                var tag = document.createElement("script");
-                tag.src = act2[1];
-                tag.type = "module" 
-                document.getElementById("extention_import").appendChild(tag);
-                console.log("[extention loader] " + act2[1] + " loaded")
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-        } else{}
-    
-    });
-    var tag = document.createElement("script");
-    tag.src = "core.js";
-    tag.type = "module" 
-    document.getElementById("extention_import").appendChild(tag);
-    return true
+        var ext = localStorage.getItem("extention")
+        var act = ext.split(",")
+        act.forEach((main) => {
+            if (main != "" && getCookie("geoloup") != null) {
+                var act2 = main.split(";")
+                fetch(act2[1], { method: "GET" })
+                    .then((response) => response.text())
+                    .then((data) => {
+                        var tag = document.createElement("script");
+                        tag.src = act2[1];
+                        tag.type = "module"
+                        document.getElementById("extention_import").appendChild(tag);
+                        console.log("[extention loader] " + act2[1] + " loaded")
+                    })
+                    .catch((error) => {
+                        console.error("Error:", error);
+                    });
+            } else { }
+
+        });
+        var tag = document.createElement("script");
+        tag.src = "core.js";
+        tag.type = "module"
+        document.getElementById("extention_import").appendChild(tag);
+        return true
     } catch {
         return null
     }
