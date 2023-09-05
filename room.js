0e7048aef943e29fa96c263f46f008865d98830d
franck403 <79372051+franck403@users.noreply.github.com>
2023-09-05 19:45:51 -0400

    other fix

--- room.js
+++ room.js
@@ -11,14 +11,14 @@ function load_image(chat_id, min_, max_) {
     return
   }
   for (i = 0; i < images.length; i++) {
-    var image = x[i]
+    var image = images[i]
     if (image.dataset.state != "load") {
       data.push(image)
     }
   }
   data.reverse()
   for (i = 0; i < data.length; i++) {
-    var image = x[i]
+    var image = images[i]
     if (image.dataset.state != "load") {
       if (min <= calc && calc <= (min + max)) {
         image.src = image.dataset.src
