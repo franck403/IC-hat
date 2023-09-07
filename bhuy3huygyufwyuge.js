27dac946cc84eabe11265225682eda516bd684d5
franck403 <79372051+franck403@users.noreply.github.com>
2023-09-07 11:58:01 -0400

    added a function back that was deleted

--- bhuy3huygyufwyuge.js
+++ bhuy3huygyufwyuge.js
@@ -191,6 +191,20 @@ export function message_render(message,type="none") {
   }
 }
 
+export function time_fresh() {
+  try {
+    var list = document.getElementsByClassName("people-person")
+    Object.keys(list).forEach(id => {
+      var el = list[id]
+      var elt = document.getElementById("time_" + el.dataset.chatid)
+      var elf = parseFloat(elt.dataset.send)
+      elt.innerHTML = message_date(elf,el.dataset.chatid)
+    });
+  } catch {}
+  setTimeout(time_fresh, 1000);
+}
+
+setTimeout(time_fresh, 30000);
 
 export function changepic(img,email) {
   let formData = new FormData();
