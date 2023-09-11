089727081492f0a53d568f4382fe45e9680267e7
franck403 <79372051+franck403@users.noreply.github.com>
2023-09-11 12:38:24 -0400

    send

--- core.js
+++ core.js
@@ -140,6 +140,7 @@ export function image_render(email, name) {
             document.getElementById("file").style.display = "none";
             document.getElementById("file_input").value = "";
         }
+        reader.readAsDataURL(file)
     });
 }
 try {
