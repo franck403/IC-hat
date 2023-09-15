858644741d13653c3697df357e0d3e117c8f1746
franck403 <79372051+franck403@users.noreply.github.com>
2023-09-15 09:16:22 -0400

    asdadas

--- home.js
+++ home.js
@@ -5,32 +5,7 @@ fetch("https://I-home.calebgauthier.repl.co/text.html")
     document.getElementById("homedata").innerHTML = ldata
 })
 .catch((error) => {});
-var login = document.getElementById("login");
-var register = document.getElementById("register");
-var start = document.getElementById("start");
-var log_out = document.getElementById("out");
-var home = netlifyIdentity.currentUser()
-if (home != null) {
-    start.addEventListener('click', (e) => {
-        if (window.location.origin.endsWith("/")) {
-            window.location.replace(`${window.location.origin}chat`)
-        } else {
-            window.location.replace(`${window.location.origin}/chat`)
-        }
-    });
-    log_out.addEventListener('click', (e) => {
-        netlifyIdentity.logout()
-    });
-    document.getElementById("no").remove()
-} else {
-    login.addEventListener('click', (e) => {
-        netlifyIdentity.open("login")
-    });
-    register.addEventListener('click', (e) => {
-        netlifyIdentity.open("signup")
-    });
-    document.getElementById("yes").remove()
-}
-if (window.location.href.endsWith("#")) {
-    window.location.replace(`${window.location.origin}/chat`)
+var iFrameDetection = (window === window.parent) ? false : true;
+if (!iFrameDetection) {
+    document.location.replace(document.location.origin)
 }
