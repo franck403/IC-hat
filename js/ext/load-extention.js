f1adabc199bcc244012223f0273bf06f00c76f98
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-07 09:24:41 -0400

    fccvvvvvvvvfggfgftffgffftfg

--- js/ext/load-extention.js
+++ js/ext/load-extention.js
@@ -32,11 +32,12 @@ onChildAdded(library, (data) => {
     var extName = val.name
     var description = val.description
     var creator = val.user_name
-    var email = val.email
-    `   
+    var email = val["email"]
+    var elem = `   
     <div class="store-2 store_box" data-name="${extName}" data-content="${content}" id="add_extention">
         <img class="store_img" src="img/ic-hat.png"></img>
         <button class="store_button">Add crypt message</button>
     </div>
     `
+    document.getElementById(store).innerHTML = document.getElementById(store).innerHTML + elem
 });
