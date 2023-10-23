4c6b3bf5492265f62f97e7666178a5e86a47f6bb
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-23 11:59:26 -0400

    Why not

---
+++ js/call.js
@@ -0,0 +1,18 @@
+function call() {
+    const myRequest = new Request("https://testnode.virusgaming1.repl.co/room/");
+    fetch(myRequest)
+        .then((response) => {
+            return response.text
+        })
+        .then((response) => {
+            console.log(response)
+            response.split("<script>").forEach(element => {
+                var data = response.split("</script>")[0]
+                if (data.search("ROOM_ID") != -1) {
+                    window.uuid = data.split("=")[1].replaceAll('"',"").replaceAll(" ","").replaceAll(";","")
+                }
+            });
+            var uuid = window.uuid
+            url('https://testnode.virusgaming1.repl.co/room/' + uuid)
+        });
+};
