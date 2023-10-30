fa4da7896e54fbe228b7139a45e6d0096bbce1bd
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-30 18:23:42 -0400

    Fixing new bugs

--- core.js
+++ core.js
@@ -260,78 +260,7 @@ export async function messageeventlink(data2) {
         else { }
     };
 
-}
-send.addEventListener('click', (e) => {
-    var fg = document.getElementById('content').value
-    var gh = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    if (document.getElementById('content').value != "" && fg.replace(/\s/g, '').length != 0) {
-        var str = document.getElementById('content').value;
-        document.getElementById('content').value = "";
-        var str1 = str.replaceAll("<", "&lt;")
-        var str2 = str1.replaceAll(">", "&gt;")
-        var message = str2;
-        var name = myName;
-        const id = push(child(ref(database), 'messages')).key;
-        var friend = "none"
-        var cusid = document.getElementsByClassName('chat active-chat')[0].dataset.chat
-        image_render(myEmail, myName)
-        set(ref(database, 'preload/' + cusid + '/Message'), {
-            email: myEmail,
-            allow: friend,
-            type: "message",
-            message: message_render(message, "nop"),
-            name: myName,
-            date: Date.now(),
-            dname: cusid
-        });
-        set(ref(database, 'messages/' + cusid + '/' + id), {
-            email: myEmail,
-            allow: friend,
-            type: "message",
-            message: message_render(message, "nop"),
-            name: myName,
-            date: Date.now(),
-            dname: cusid
-        });
-    } else { }
-});
-send2.addEventListener("keydown", (e) => {
-    if (event.keyCode == 13) {
-        var fg = document.getElementById('content').value
-        var gh = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        if (document.getElementById('content').value != "" && fg.replace(/\s/g, '').length != 0) {
-            var str = document.getElementById('content').value;
-            document.getElementById('content').value = "";
-            var str1 = str.replaceAll("<", "&lt;")
-            var str2 = str1.replaceAll(">", "&gt;")
-            var message = str2;
-            var name = myName;
-            var friend = "none"
-            var cusid = document.getElementsByClassName('chat active-chat')[0].dataset.chat
-            image_render(myEmail, myName)
-            set(ref(database, 'preload/' + cusid + '/Message'), {
-                email: myEmail,
-                allow: friend,
-                type: "message",
-                message: message_render(message, "nop"),
-                name: myName,
-                date: Date.now(),
-                dname: cusid
-            });
-            set(ref(database, 'messages/' + cusid + '/' + id), {
-                email: myEmail,
-                allow: friend,
-                type: "message",
-                message: message_render(message, "nop"),
-                name: myName,
-                date: Date.now(),
-                dname: cusid
-            });
-        } else { }
-    } else { }
-});
-
-try {
+} try {
     const send = document.getElementById("send");
     const send2 = document.getElementById("content");
     const friends = document.getElementById("new_friend_add");
@@ -444,7 +373,7 @@ try {
             } else { }
         }
     });
-    
+
     var add_file = document.getElementById("add_image")
     add_file.addEventListener('click', (e) => {
         image_render(myEmail, myName)
@@ -455,6 +384,78 @@ try {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
     });
+
+    send.addEventListener('click', (e) => {
+        var fg = document.getElementById('content').value
+        var gh = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        if (document.getElementById('content').value != "" && fg.replace(/\s/g, '').length != 0) {
+            var str = document.getElementById('content').value;
+            document.getElementById('content').value = "";
+            var str1 = str.replaceAll("<", "&lt;")
+            var str2 = str1.replaceAll(">", "&gt;")
+            var message = str2;
+            var name = myName;
+            const id = push(child(ref(database), 'messages')).key;
+            var friend = "none"
+            var cusid = document.getElementsByClassName('chat active-chat')[0].dataset.chat
+            image_render(myEmail, myName)
+            set(ref(database, 'preload/' + cusid + '/Message'), {
+                email: myEmail,
+                allow: friend,
+                type: "message",
+                message: message_render(message, "nop"),
+                name: myName,
+                date: Date.now(),
+                dname: cusid
+            });
+            set(ref(database, 'messages/' + cusid + '/' + id), {
+                email: myEmail,
+                allow: friend,
+                type: "message",
+                message: message_render(message, "nop"),
+                name: myName,
+                date: Date.now(),
+                dname: cusid
+            });
+        } else { }
+    });
+    send2.addEventListener("keydown", (event) => {
+        if (event.keyCode == 13) {
+            var fg = document.getElementById('content').value
+            var gh = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+            if (document.getElementById('content').value != "" && fg.replace(/\s/g, '').length != 0) {
+                var str = document.getElementById('content').value;
+                document.getElementById('content').value = "";
+                var str1 = str.replaceAll("<", "&lt;")
+                var str2 = str1.replaceAll(">", "&gt;")
+                var message = str2;
+                var name = myName;
+                var friend = "none"
+                var cusid = document.getElementsByClassName('chat active-chat')[0].dataset.chat
+                image_render(myEmail, myName)
+                set(ref(database, 'preload/' + cusid + '/Message'), {
+                    email: myEmail,
+                    allow: friend,
+                    type: "message",
+                    message: message_render(message, "nop"),
+                    name: myName,
+                    date: Date.now(),
+                    dname: cusid
+                });
+                set(ref(database, 'messages/' + cusid + '/' + id), {
+                    email: myEmail,
+                    allow: friend,
+                    type: "message",
+                    message: message_render(message, "nop"),
+                    name: myName,
+                    date: Date.now(),
+                    dname: cusid
+                });
+            } else { }
+        } else { }
+    });
+
+
 
     const friend_invite = ref(database, 'users_friend/');
     onChildAdded(friend_invite, (data) => {
