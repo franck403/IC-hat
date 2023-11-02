0dc1f576d55c06b554e6853d686ee462cf3b1642
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-02 12:15:11 -0400

    dsdaaa

--- core.js
+++ core.js
@@ -505,7 +505,7 @@ try {
             //localStorage.setObj("roomlist",localStorage.getObj("roomlist").push([data.val().dname]))
             onChildChanged(ref(database, 'preload/' + dnamef + '/Message'), async (data2) => {
                 console.log("Child changed")
-                console.log(data2.val())
+                console.log(data2.val().type)
                 if (data2.val().type == "call") {
                     console.log("Geting call")
                     window.receive(`?f&type=call&uuid=${data2.val().message}&name=${data2.val().name}&`)
