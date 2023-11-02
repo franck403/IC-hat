d5f243a2246bf335a6cd0eae7a10f3cbacdd1c7a
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-02 12:20:35 -0400

    Fixed a bug

--- js/call.js
+++ js/call.js
@@ -1,11 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
 import {
-    ref,
-    set
+    getDatabase,
+    set,
+    ref
 } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+localStorage.setItem("state", "no")
+const firebaseConfig = {
+    apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+    authDomain: "ic-hat.firebaseapp.com",
+    databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+    projectId: "ic-hat",
+    storageBucket: "ic-hat.appspot.com",
+    messagingSenderId: "720687529085",
+    appId: "1:720687529085:web:2d964e880c5e2398058514",
+    measurementId: "G-YC8K0D7GLR"
+};
 
-var app = window.appFire
-var database = window.databaseFire
-
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 /*
     email: myEmail,
