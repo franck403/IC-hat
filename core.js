f4b2020f26f78d7b814db2b2cf3e0debee3bebfe
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-15 11:06:10 -0500

    adadasdadsadasdasdsadsdsdasdasdsadasdasda

--- core.js
+++ core.js
@@ -497,23 +497,37 @@ try {
     window.MessageWorkerLoop = MessageWorkerLoop
     async function MessageWorker() {
         console.log("[Message worker] Loading message")
-        for (let i = 0; i < (window.processingMessage.length/2); i++) {
+        for (let i = 0; i < (window.processingMessage.length / 2); i++) {
             console.log("[Message worker] Chargin message")
             try {
                 try {
-                    var date1 = Date(window.processingMessage[window.processingMessage[i]][window.processingMessage[window.processingMessage[i]].length - 1][0].val().date)
-                    var date2 = Date(window.processingMessage[window.processingMessage[i]][window.processingMessage[window.processingMessage[i]].length - 4][0].val().date)
-                    if (date1 < date2) {
-                        window.processingMessage[window.processingMessage[i]].reverse()
+                    try {
+                        var date1 = Date(window.processingMessage[window.processingMessage[i]][0][0].val().date)
+                        var date2 = Date(window.processingMessage[window.processingMessage[i]][window.processingMessage[window.processingMessage[i]].length - 4][0].val().date)
+                        console.log("[Message worker]" + date1)
+                        console.log("[Message worker]" + date2)
+                        if (date1 < date2) {
+                            window.processingMessage[window.processingMessage[i]].reverse()
+                        }
+                    } catch {
+                        var date1 = Date(window.processingMessage[window.processingMessage[i]][window.processingMessage[window.processingMessage[i]].length - 1][0].val().date)
+                        var date2 = Date(window.processingMessage[window.processingMessage[i]][window.processingMessage[window.processingMessage[i]].length - 4][0].val().date)
+                        console.log("[Message worker]" + date1)
+                        console.log("[Message worker]" + date2)
+                        if (date1 < date2) {
+                            window.processingMessage[window.processingMessage[i]].reverse()
+                        }
                     }
                 } catch (err) {
+                    console.log("[Message worker]" + date1)
+                    console.log("[Message worker]" + date2)
                     var date1 = Date(window.processingMessage[window.processingMessage[i]][0][0].val().date)
                     var date2 = Date(window.processingMessage[window.processingMessage[i]][3][0].val().date)
                     if (date1 < date2) {
                         window.processingMessage[window.processingMessage[i]].reverse()
                     }
-                        }
-            } catch (err){
+                }
+            } catch (err) {
                 window.processingMessage[window.processingMessage[i]].reverse()
             }
             console.log("[Message worker]" + date1)
