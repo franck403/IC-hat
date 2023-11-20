092e9ec53061fcf65b228870f0b85fcadfc54a00
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-20 10:32:26 -0500

    wokring on the extention fix

---
+++ js/ext/store.extention.js
@@ -0,0 +1,24 @@
+const putInCache = async (request) => {
+    response = await fetch(request)
+    if (url.search("api/extention/") != -1) {
+        const cache = await caches.open("extention");
+        var id = url.split("/").reverse()[0]
+        await cache.put(request, response);
+        }
+};
+
+const cacheFirst = async (request) => {
+
+    const url = new URL(request.url);
+    var cache = caches.open("extention")
+    var responseFromCache = await cache.match(request)
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
+});
