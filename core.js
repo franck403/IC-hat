105e2c02e3a0362cee15b92e8454292662eea539
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-20 10:51:24 -0500

    ?

--- core.js
+++ core.js
@@ -461,7 +461,7 @@ try {
         }
     });
     function MessageWorkerLoop(snapshot, back) {
-
+        console.log(snapshot)
         for (let i = 0; i < (snapshot.length); i++) {
             var data = snapshot[i]
             var data2 = data[0]
