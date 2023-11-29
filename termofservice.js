ff5f845a5121ec7875818ec7f3776f8dfc68b603
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-29 12:33:42 -0500

    d

--- termofservice.js
+++ termofservice.js
@@ -1,18 +1,21 @@
-var mm = document.getElementsByClassName("mm")[0];
-var span = document.getElementsByClassName("cls")[0];
-var checked = netlifyIdentity.gotrue.currentUser().user_metadata.termofservice
-if (checked === "yes") {
-    mm.style.display = "none";
-} else {
-    mm.style.display = "block";
-};
+function mainds() {
+    var mm = document.getElementsByClassName("mm")[0];
+    var span = document.getElementsByClassName("cls")[0];
+    var checked = netlifyIdentity.gotrue.currentUser().user_metadata.termofservice
+    if (checked === "yes") {
+        mm.style.display = "none";
+    } else {
+        mm.style.display = "block";
+    };
 
-function acpt() {
-    mm.style.display = "none";
-    netlifyIdentity.gotrue.currentUser().update({
-        data: {
-            termofservice: 'yes',
-            term:'Welcome to Ic-hat! These Terms of Service govern your use and outline the rights and responsibilities between you and us. By accessing or using the Application, you agree to be bound by these Terms. Please take note that the'
-        }
-    })
-};
+    function acpt() {
+        mm.style.display = "none";
+        netlifyIdentity.gotrue.currentUser().update({
+            data: {
+                termofservice: 'yes',
+                term: 'Welcome to Ic-hat! These Terms of Service govern your use and outline the rights and responsibilities between you and us. By accessing or using the Application, you agree to be bound by these Terms. Please take note that the'
+            }
+        })
+    };
+}
+setTimeout(mainds, 1000)
