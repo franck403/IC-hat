3d7b55a9b701a59402c8fadc03d7c0d73f02dd8e
franck403 <79372051+franck403@users.noreply.github.com>
2023-11-29 12:09:16 -0500

    ddddddd

--- termofservice.js
+++ termofservice.js
@@ -1,6 +1,6 @@
 var mm = document.getElementsByClassName("mm")[0];
 var span = document.getElementsByClassName("cls")[0];
-var checked = 'no'
+var checked = netlifyIdentity.currentUser().user_metadata.termofservice
 if (checked === "yes") {
     mm.style.display = "none";
 } else {
@@ -9,4 +9,10 @@ if (checked === "yes") {
 
 function acpt() {
     mm.style.display = "none";
+    netlifyIdentity.gotrue.currentUser().update({
+        data: {
+            termofservice: 'yes',
+            term:'Welcome to Ic-hat! These Terms of Service govern your use and outline the rights and responsibilities between you and us. By accessing or using the Application, you agree to be bound by these Terms. Please take note that the'
+        }
+    })
 };
