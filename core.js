f614a8171b2716d832aa4db875ca2e0eeba0e915
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-30 13:50:50 -0500

    removing scroll

--- core.js
+++ core.js
@@ -425,9 +425,10 @@ try {
                 }
             })
             document.getElementById("room_" + el.id.replace("d", "")).addEventListener("scroll", (e) => {
+                preventDefault(e);
                 for (let i = 0; i < (window.processingMessage.length); i++) {
                     if (findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).length > 1) {
-                        var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice(0, 1)
+                        var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice(0, 10)
                     } else {
                         var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice()
                     }
