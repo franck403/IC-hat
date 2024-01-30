4cef32a0db8a80b6a15a022d9dff60d4053e0558
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-30 11:01:53 -0500

    more code

--- core.js
+++ core.js
@@ -495,13 +495,13 @@ try {
                 console.log("[Message worker] Chargin message")
                 if (select != undefined && select == i) {
                     if (findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).length > 20) {
-                        var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice(0, 20).reverse()
+                        var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice().reverse().slice(0, 20).reverse()
                     } else {
                         var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice().reverse()
                     }    
                 }
                 if (findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).length > 20) {
-                    var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice(0, 20).reverse()
+                    var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice().reverse().slice(0, 20).reverse()
                 } else {
                     var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice().reverse()
                 }
