a79329db480f93b634e662f4f0f8e4fa04ad5913
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-30 12:59:44 -0500

    ADding more timeout

--- core.js
+++ core.js
@@ -442,6 +442,10 @@ try {
             })
             el.dataset.enable = true
             setTimeout(MessageLoad, 1000);
+            setTimeout(() => {
+                elem.scrollTop = elem.scrollHeight;
+                elem.scrollTop = elem.scrollHeight;                    
+            }, 2000);
         }
     });
     function MessageWorkerLoop(snapshot, back) {
@@ -461,8 +465,6 @@ try {
                         d1.innerHTML = message[1] + d1.innerHTML
                     }
                     var elem = d1
-                    elem.scrollTop = elem.scrollHeight;
-                    elem.scrollTop = elem.scrollHeight;
                 }
             }
         }
