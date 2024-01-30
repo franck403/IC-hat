4a2ad10d3d8f04d1fd145365d8c4b13796a5f67a
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-30 15:04:40 -0500

    Removing scroll message loading

--- core.js
+++ core.js
@@ -425,6 +425,7 @@ try {
                 }
             })
             document.getElementById("room_" + el.id.replace("d", "")).addEventListener("scroll", (e) => {
+                /*
                 for (let i = 0; i < (window.processingMessage.length); i++) {
                     if (findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).length > 1) {
                         var snapshot = findAll((obj => obj[1] !== true), window.processingMessage[window.processingMessage[i]]).slice(0, 10)
@@ -435,6 +436,7 @@ try {
                     e.preventDefault();
                     window.processingMessage[window.processingMessage[i]] = window.processingMessage[window.processingMessage[i]].slice(0,findAll((obj => obj[1] === true), window.processingMessage[window.processingMessage[i]]).length).concat(resultSnapshot).concat(window.processingMessage[window.processingMessage[i]].slice((window.processingMessage[window.processingMessage[i]].slice(0,findAll((obj => obj[1] === true), window.processingMessage[window.processingMessage[i]]).length)).length + snapshot.length))
                 }
+                */
             })
             el.dataset.enable = true
             setTimeout(MessageLoad, 1000);
