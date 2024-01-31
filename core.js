271888fd4dedff6e5921bfbf2dc3e701f7534db3
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-31 11:46:18 -0500

    Fixing bugs

--- core.js
+++ core.js
@@ -659,7 +659,7 @@ try {
                         document.getElementById(`time_${dnamef}`).innerHTML = date
                         document.getElementById(`prew_${dnamef}`).innerHTML = message_render(data2.val().message)
                     }
-                    MessageLoad()
+                    MessageLoad(undefined,max)
                 }
             })
             onChildAdded(ref(database, 'preload/' + dnamef), async (data2) => {
