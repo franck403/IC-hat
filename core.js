26b765e448654a088521cb3a09de5c6cac32cf9d
franck403 <79372051+franck403@users.noreply.github.com>
2024-02-01 08:09:47 -0500

    wait database update

--- core.js
+++ core.js
@@ -659,10 +659,12 @@ try {
                         document.getElementById(`time_${dnamef}`).innerHTML = date
                         document.getElementById(`prew_${dnamef}`).innerHTML = message_render(data2.val().message)
                     }
-                    MessageLoad(undefined,3)
-                    var elem = document.getElementById(`roo_${dnamef}`)
-                    elem.scrollTop = elem.scrollHeight;
-                    elem.scrollTop = elem.scrollHeight;         
+                    setTimeout(()=> {
+                        MessageLoad(undefined,3)
+                        var elem = document.getElementById(`room_${dnamef}`)
+                        elem.scrollTop = elem.scrollHeight;
+                        elem.scrollTop = elem.scrollHeight;             
+                    },2000)
                 }
             })
             onChildAdded(ref(database, 'preload/' + dnamef), async (data2) => {
