746f38eae71f5f9ec631337520f58a68bebc5a95
franck403 <79372051+franck403@users.noreply.github.com>
2024-07-06 17:42:35 -0400

    Making style better

--- js/disccusion/setting.js
+++ js/disccusion/setting.js
@@ -7,22 +7,7 @@ class MyCustomElement extends HTMLElement {
         this.uuid = crypto.randomUUID()
         const uuid = this.uuid
         var script = 'popupSettingMenuShow("d' + uuid + '")'
-        var style = `
-        background-color: #fff;
-        color: #1f194c;
-        max-width: 50vw;
-        width: 50vw;
-        z-index: 10;
-        padding: 4px;
-        font-size: 20px;
-        display: flex;
-        flex-wrap: wrap;
-        position: relative;
-        margin-right: -70px;
-        border-radius: 5px;
-        border: 1px solid #00000030;
-        visibility: hidden;`
-        var div = `<div id='${this.uuid}'><style>${style}</style><i class="fa-solid fa-ellipsis"style="rotate: 90deg;" onclick="popupSettingMenuShow('d${uuid}')"></i><div id='d${uuid}' style="visibility:hidden;" class=context-menu><span class=item onclick="createInviteDiscusion()"><i class="fa-solid fa-user-plus"></i>Create invite</span><span class=item onclick='hidediscusion()'><i class="fa-solid fa-user-minus"></i>Hide for you</span><span class=item onclick="ChangeDisplayName()"><i class="fa-solid fa-pen-to-square"></i>Edit name</span></div></div>`;
+        var div = `<div id='${this.uuid}'><i class="fa-solid fa-ellipsis"style="rotate: 90deg;" onclick="popupSettingMenuShow('d${uuid}')"></i><div id='d${uuid}' style="visibility:hidden;" class=context-menu><span class=item onclick="createInviteDiscusion()"><i class="fa-solid fa-user-plus"></i>Create invite</span><span class=item onclick='hidediscusion()'><i class="fa-solid fa-user-minus"></i>Hide for you</span><span class=item onclick="ChangeDisplayName()"><i class="fa-solid fa-pen-to-square"></i>Edit name</span></div></div>`;
         this.innerHTML = div
     }
 
