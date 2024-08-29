d5611cc89862aded2d5f3dfcf695101b9b166cb1
franck403 <79372051+franck403@users.noreply.github.com>
2024-08-29 11:59:24 -0400

    updateing api

--- image.js
+++ image.js
@@ -4,19 +4,22 @@ const storage = getStorage();
 const storageRef = ref(storage, 'some-child');
 
 // 'file' comes from the Blob or File API
-function firebaseUpladeBytes(blob) {
+function firebaseUpladeBytes(blob,path) {
     const storage = getStorage();
-    const storageRef = ref(storage, 'uplode');
+    const storageRef = ref(storage, 'uplode/' + path);
     uploadBytes(storageRef, blob).then((snapshot) => {
         console.log('Uploaded a blob or file!');
       });      
 }
 
-
+// 'base64' is a image string
 function firebaseUpladeBase64(base64) {
     const storage = getStorage();
-    const storageRef = ref(storage, 'uplode');
+    const storageRef = ref(storage, 'uplode' + path);
     uploadString(storageRef, base64, 'base64url').then((snapshot) => {
         console.log('Uploaded a base64url string!');
     });
 }
+
+window.firebaseUpladeBytes  = firebaseUpladeBytes
+window.firebaseUpladeBase64 = firebaseUpladeBase64
