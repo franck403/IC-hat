f261a6d63bf34f680b3a3a1e140c7c3ded2283bc
franck403 <79372051+franck403@users.noreply.github.com>
2024-08-29 12:04:57 -0400

    image.js fix

--- image.js
+++ image.js
@@ -1,4 +1,17 @@
 import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyD9po7l-vwO0VrY1rMYDFTYNlEBv54T6do",
+    authDomain: "ic-hat.firebaseapp.com",
+    databaseURL: "https://ic-hat-default-rtdb.firebaseio.com",
+    projectId: "ic-hat",
+    storageBucket: "ic-hat.appspot.com",
+    messagingSenderId: "720687529085",
+    appId: "1:720687529085:web:2d964e880c5e2398058514",
+    measurementId: "G-YC8K0D7GLR"
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const storage = getStorage();
 const storageRef = ref(storage, 'some-child');
