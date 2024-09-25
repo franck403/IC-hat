f0a34a843cacdf96917d8881851744cdcdec8694
franck403 <79372051+franck403@users.noreply.github.com>
2024-09-25 09:33:07 -0400

    ug gifjnfrwnjfgvrnjfvrnhj

--- core.js
+++ core.js
@@ -653,10 +653,10 @@ try {
                 console.log(autoReversed)
                 console.log(reversed)
                 if (reversed || autoReversed) {
-                    var resultSnapshot = MessageWorkerLoop(snapshot.slice(0, snapshot.length), true)
+                    var resultSnapshot = MessageWorkerLoop(snapshot.slice(0, snapshot.length), false)
                     window.processingMessage[window.processingMessage[i]] = resultSnapshot.concat((window.processingMessage[window.processingMessage[i]].slice(snapshot.length)))
                 } else {
-                    var resultSnapshot = MessageWorkerLoop(snapshot.slice(0, snapshot.length), false)
+                    var resultSnapshot = MessageWorkerLoop(snapshot.slice(0, snapshot.length), true)
                     window.processingMessage[window.processingMessage[i]] = resultSnapshot.concat(window.processingMessage[window.processingMessage[i]].slice(snapshot.length))
                 }
             }
