dc6cff34726b583028049b5330f516ba45e78347
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-01-24 15:19:29 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -210,8 +210,8 @@ export function embed_render(message) {
   var message_start = message_good.substring(0, 1000);
   var message_start = message_start.replaceAll('<','&lt;')
   var message_start = message_start.replaceAll('>','&gt;')
+  console.log(message_start)
   var message_start = message_start.replaceAll('\n','<br>')
-  var message_start = message_start.replaceAll('&lt;br&gt;','<br>')
 
   if (message_start == "undefined" || message_start == undefined) {
     return null; 
