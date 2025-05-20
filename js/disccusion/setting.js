a1dee2d2ee568bfb48b7580c0e08dbf34734e5fb
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-05-20 13:10:35 -0400

    bug ficx

--- js/disccusion/setting.js
+++ js/disccusion/setting.js
@@ -229,7 +229,7 @@ function dataURItoBlob(dataURI) {
 
 async function loopThroughElements(parent) {
     for (let child of parent.children) {
-        if (child.src.trim() == '' && child.innerHTML.trim() == '') {
+        if (child.tagName == 'P' && child.innerHTML.trim() == '') {
             console.log('Removed : ' +  child.tagName.toUpperCase() == 'P' && child.innerHTML.trim() == '')
             child.remove()
         } else if (child.children.length > 0) {
